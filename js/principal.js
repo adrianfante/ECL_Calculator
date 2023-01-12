8b81b0067791f65d42d243fb4fa28b19d77f111a
adrianfante <adrianfante@gmail.com>
2023-01-12 10:26:55 -0300

    calcula Stage2 hasta 6 años

--- js/principal.js
+++ js/principal.js
@@ -25,28 +25,50 @@ const results = document.querySelector("#results");
 function updateTable(){
     let procDate = new Date(processDate.value);
     let cancDate = new Date(cancellationDate.value);
-    let resMat = (cancDate.getTime() - procDate.getTime())/31536000000.0;
+    let resMat = Math.round((cancDate.getTime() - procDate.getTime())/315360000.0)/100.0;
     let lgd = .72;
     let ECLStage1 = 0;
     let ECLStage2 = 0;
 
-    console.log("actualizamos la tabla");
 
     alasql.promise('select MODEL from XLSX("data/industry.xlsx") where Local_Code = ? ', parseInt(indCode.value ))
         .then((res1) => {
-            let model = res1[0].MODEL
+            let model = res1[0].MODEL;
             alasql.promise('select PD_0, PD_1, PD_2, PD_3,PD_4, PD_5, PD_6, PD_7, PD_8, PD_9, PD_10, PD_11, PD_12, PD_13, PD_14 from XLSX("data/pds.xlsx") where MODEL = "' + model  + '" and CRR2 = "' + crr.value + '"')
                 .then((res2) => {
                     if (resMat >= 1) {
-                        ECLStage1 = res2[0].PD_0.toFixed(4)* lgd * parseFloat(ead1.value) * Math.pow(1 + parseFloat(intRate.value)/100.0, -0.5);
+                        ECLStage1 = res2[0].PD_0.toFixed(4) * lgd * parseFloat(ead1.value) * Math.pow(1 + parseFloat(intRate.value)/100.0, -0.5);
+                        ECLStage2 = ECLStage1;
+                        if (resMat >= 2) {
+                            ECLStage2 = ECLStage2 + res2[0].PD_1.toFixed(4) * lgd * parseFloat(ead2.value) * Math.pow(1 + parseFloat(intRate.value)/100.0, -1.5);
+                            if (resMat >=3) {
+                                ECLStage2 = ECLStage2 + res2[0].PD_2.toFixed(4) * lgd * parseFloat(ead3.value) * Math.pow(1 + parseFloat(intRate.value)/100.0, -2.5);
+                                if (resMat >=4) {
+                                    ECLStage2 = ECLStage2 + res2[0].PD_3.toFixed(4) * lgd * parseFloat(ead4.value) * Math.pow(1 + parseFloat(intRate.value)/100.0, -3.5);
+                                    if (resMat >=5) {
+                                        ECLStage2 = ECLStage2 + res2[0].PD_4.toFixed(4) * lgd * parseFloat(ead5.value) * Math.pow(1 + parseFloat(intRate.value)/100.0, -4.5);
+                                        if (resMat >=6) {
+                                            ECLStage2 = ECLStage2 + res2[0].PD_5.toFixed(4) * lgd * parseFloat(ead6.value) * Math.pow(1 + parseFloat(intRate.value)/100.0, -5.5);
+
+                                        } else {
+                                        ECLStage2 = ECLStage2 + res2[0].PD_5.toFixed(4) * lgd * parseFloat(ead6.value) * (resMat - 5) * Math.pow(1 + parseFloat(intRate.value)/100.0, -(5+(resMat-5)/2.0));
+                                        }
+                                    } else {
+                                    ECLStage2 = ECLStage2 + res2[0].PD_4.toFixed(4) * lgd * parseFloat(ead5.value) * (resMat - 4) * Math.pow(1 + parseFloat(intRate.value)/100.0, -(4+(resMat-4)/2.0));
+                                    }
+                                } else {
+                                ECLStage2 = ECLStage2 + res2[0].PD_3.toFixed(4) * lgd * parseFloat(ead4.value) * (resMat - 3) * Math.pow(1 + parseFloat(intRate.value)/100.0, -(3+(resMat-3)/2.0));
+                                }
+                            } else {
+                            ECLStage2 = ECLStage2 + res2[0].PD_2.toFixed(4) * lgd * parseFloat(ead3.value) * (resMat - 2) * Math.pow(1 + parseFloat(intRate.value)/100.0, -(2+(resMat-2)/2.0));
+                            }
+                        } else {
+                            ECLStage2 = ECLStage2 + res2[0].PD_1.toFixed(4) * lgd * parseFloat(ead2.value) * (resMat - 1) * Math.pow(1 + parseFloat(intRate.value)/100.0, -(1+(resMat-1)/2.0));
+                        }
                     } else {
                         ECLStage1 = res2[0].PD_0.toFixed(4) * lgd * parseFloat(ead1.value) * resMat * Math.pow(1 + parseFloat(intRate.value)/100.0, -resMat/2.0);
+                        ECLStage2 = ECLStage1;
                     }
-                    ECLStage1.toFixed(2);
-                    console.log(ECLStage1);
-                    console.log(crr.value);
-                    console.log(model);
-                    console.log(res2[0].PD_0);
                     const fila = "<td>" + custName.value + "</td><td>" + intRate.value + "</td><td>" + processDate.value + "</td><td>" + cancellationDate.value  + "</td><td>" + crr.value + "</td><td>" + indCode.value + "</td><td>" + ead1.value + "</td><td>" + ead2.value + "</td><td>" + ead3.value + "</td><td>" + ead4.value + "</td><td>" + ead5.value + "</td><td>" + ead6.value + "</td><td>" + ead7.value + "</td><td>" + ead8.value + "</td><td>" + ead9.value + "</td><td>" + ead10.value + "</td><td>" + ead11.value + "</td><td>" + ead12.value + "</td><td>" + ead13.value + "</td><td>" + ead14.value + "</td><td>" + ead15.value + "</td><td>" + lgd + "</td><td>" + resMat.toFixed(2) + "</td><td>" + model + "</td><td>" + res2[0].PD_0.toFixed(4) + "</td><td>" + res2[0].PD_1.toFixed(4) + "</td><td>" + res2[0].PD_2.toFixed(4) + "</td><td>" + res2[0].PD_3.toFixed(4) + "</td><td>" + res2[0].PD_4.toFixed(4) + "</td><td>" + res2[0].PD_5.toFixed(4) + "</td><td>" + res2[0].PD_6.toFixed(4) + "</td><td>" + res2[0].PD_7.toFixed(4) + "</td><td>" + res2[0].PD_8.toFixed(4) + "</td><td>" + res2[0].PD_9.toFixed(4) + "</td><td>" + res2[0].PD_10.toFixed(4) + "</td><td>" + res2[0].PD_11.toFixed(4) + "</td><td>" + res2[0].PD_12.toFixed(4) + "</td><td>" + res2[0].PD_13.toFixed(4) + "</td><td>" + res2[0].PD_14.toFixed(4) + "</td><td>" + ECLStage1.toFixed(2) + "</td><td>" + ECLStage2.toFixed(2) + "</td>"
                     const fila2 = document.createElement("tr");
                     fila2.innerHTML = fila;
