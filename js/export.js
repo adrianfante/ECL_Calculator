801e07f30163d40cba736fe671b34b0baaf57ee6
adrianfante <adrianfante@gmail.com>
2023-01-16 17:36:23 -0300

    cambio nombre hoja excel sheet1 por ECL

--- js/export.js
+++ js/export.js
@@ -1,6 +1,6 @@
 function ExportToExcel(type, fn, dl) {
        var elt = document.getElementById('tbl_exporttable_to_xls');
-       var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+       var wb = XLSX.utils.table_to_book(elt, { sheet: "ECL" });
        return dl ?
          XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }):
          XLSX.writeFile(wb, fn || ('ECL_Calculator_report.' + (type || 'xlsx')));
